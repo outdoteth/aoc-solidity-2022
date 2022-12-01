8203856da352ae0654321076eedf131510d54731
outdoteth <outdoteth@users.noreply.github.com>
2022-12-01 14:23:21 +0000

    add part 1

--- script/day-01.s.sol
+++ script/day-01.s.sol
@@ -14,7 +14,42 @@ contract Day01Script is Script {
         rawInput = vm.readFile("./script/input/day-01.txt");
     }
 
-    function run() public {
+    function run() public view {
+        part1();
+        part2();
+    }
+
+    function part1() public view {
         string[] memory split = rawInput.split("\n");
+
+        uint256 highestCalories = 0;
+        uint256 tmpSum = 0;
+        for (uint256 i = 0; i < split.length; i++) {
+            if (split[i].equal("")) {
+                highestCalories = tmpSum > highestCalories ? tmpSum : highestCalories;
+                tmpSum = 0;
+            } else {
+                tmpSum += split[i].parseInt();
+            }
+        }
+
+        console.log("Part 1: ", highestCalories);
+    }
+
+    function part2() public view {
+        string[] memory split = rawInput.split("\n");
+
+        uint256[] memory calorieSums = new uint256[][](split.length);
+        uint256 tmpSum = 0;
+        for (uint256 i = 0; i < split.length; i++) {
+            if (split[i].equal("")) {
+                calorieSums[] tmpSum = 0;
+            } else {
+                tmpSum += split[i].parseInt();
+                console.log(split[i].parseInt());
+            }
+        }
+
+        console.log("Part 1: ", highestValue);
     }
 }
--- script/lib/StringsPlusPlus.sol
+++ script/lib/StringsPlusPlus.sol
@@ -15,4 +15,21 @@ library StringsPlusPlus {
             output[i] = s.split(delimSlice).toString();
         }
     }
+
+    function parseInt(string memory str) internal pure returns (uint256 output) {
+        bytes memory _bytesValue = bytes(str);
+        uint256 j = 1;
+        uint256 i = _bytesValue.length - 1;
+        while (i >= 0) {
+            assert(uint8(_bytesValue[i]) >= 48 && uint8(_bytesValue[i]) <= 57);
+            output += (uint8(_bytesValue[i]) - 48) * j;
+            j *= 10;
+            if (i > 0) i--;
+            else break;
+        }
+    }
+
+    function equal(string memory str, string memory cmp) internal pure returns (bool) {
+        return keccak256(abi.encodePacked(str)) == keccak256(abi.encodePacked(cmp));
+    }
 }
